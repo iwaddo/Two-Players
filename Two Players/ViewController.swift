15d4f9331f8b4afaf1d012cb381d1f8283bfea02
Ian Waddington <iwaddo@icloud.com>
2022-10-14 20:59:15 +0100

    Finished Two Player Game exercise

--- Two Players/ViewController.swift
+++ Two Players/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playerOneLabel: UILabel!
+    @IBOutlet weak var playerTwoLabel: UILabel!
+    
+    @IBOutlet weak var inputNameLabel: UILabel!
+    @IBOutlet weak var inputNameTextField: UITextField!
+    
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        inputNameTextField.isHidden = false
+        playAgainButton.isHidden = true
+        playerOneLabel.text = "Player 1:"
+        playerTwoLabel.text = "Player 2"
+        inputNameLabel.text = "Player 1:"
+        inputNameTextField.becomeFirstResponder()
     }
 
-
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        if inputNameLabel.text == "Player 1:" {
+            playerOneLabel.text = ("Player 1: \(inputNameTextField.text!)")
+            inputNameLabel.text = "Player 2:"
+            inputNameTextField.text = ""
+        } else if inputNameLabel.text == "Player 2:" {
+            playerTwoLabel.text = ("Player 2: \(inputNameTextField.text!)")
+            inputNameTextField.text = ""
+            inputNameLabel.text = ""
+            inputNameTextField.resignFirstResponder()
+            inputNameTextField.isHidden = true
+            playAgainButton.isHidden = false
+            
+        }
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+        viewDidLoad()
+    }
+    
+    
 }
 
